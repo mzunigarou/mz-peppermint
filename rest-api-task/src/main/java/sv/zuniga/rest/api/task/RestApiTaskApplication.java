f61f5593bd8f242d3e977ed77f56e89c28a51316
mzuniga <mzuniga.rodriguez@gmail.com>
2019-12-10 11:46:17 -0600

    First commit

---
+++ rest-api-task/src/main/java/sv/zuniga/rest/api/task/RestApiTaskApplication.java
@@ -0,0 +1,13 @@
+package sv.zuniga.rest.api.task;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApiTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestApiTaskApplication.class, args);
+	}
+
+}
---
+++ rest-api-task/src/test/java/sv/zuniga/rest/api/task/RestApiTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package sv.zuniga.rest.api.task;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApiTaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
